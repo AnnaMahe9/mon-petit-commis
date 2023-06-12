f28b68f665dee36ab077ef39587a006ecdbd2ff0
Anna Mahe <annamahe9@gmail.com>
2023-06-12 23:35:44 +0200

    started homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import './Header.css';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-
+        <Header />
+        <HomePage />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,9 @@
-import React from "react"
+import React from "react";
 
 export default function Header() {
   return (
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
+    <header className="main-header">
+      <img id="user-photo" src="/images/user.png" alt="user's avatar"/>
+    </header>
   )
 }
