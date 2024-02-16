6a1c625b823d76b9913c08bb453c58a962923fac
melvynrouault <melvyn.rouault@gmail.com>
2024-02-16 16:32:46 +0100

    change recipes display

--- src/components/recipecard/RecipeCard.jsx
+++ src/components/recipecard/RecipeCard.jsx
@@ -4,19 +4,31 @@ import "./recipecard.css";
 
 export default function RecipeCard({props}) {
     console.log(props)
-    return(
-        <HashLink to={`/recipes/${props.id}`}>
-            <div className="recipe">
-                {props.photoPath ? 
-                    <img className="recipe-photo" src={`https://res.cloudinary.com/dppjibpjb/image/upload/f_auto,q_auto/v1/${props.photoPath}`} alt="recipePhoto" />
-                    :
-                    <img className="recipe-photo" src="/images/recipe1.jpg" alt="recipePhoto" />
-                }
+    const truncateText = (description, maxTextLength) => {
+        // console.log('oui oui', description)
+        let truncatedText = description;
+        if(description.length > maxTextLength) {
+            truncatedText = description.substring(0, maxTextLength) + "..."
+        }
+        return truncatedText;
+    }
 
-                <div className="recipe-infos">
-                    <h4>{props.title}</h4>
+    return(
+        <div className="recipe-card-container">
+            <HashLink to={`/recipes/${props.id}`}>
+                <div className="recipe">
+                    {props.photoPath ? 
+                        <img className="recipe-photo" src={`https://res.cloudinary.com/dppjibpjb/image/upload/f_auto,q_auto/v1/${props.photoPath}`} alt="recipePhoto" />
+                        :
+                        <img className="recipe-photo" src="/images/recipe1.jpg" alt="recipePhoto" />
+                    }
+
+                    <div className="recipe-slug">
+                        <h4>{props.title}</h4>
+                        <p>{truncateText(props.description, 30)}</p>
+                    </div>
                 </div>
-            </div>
-        </HashLink>
+            </HashLink>
+        </div>
     )
 }
